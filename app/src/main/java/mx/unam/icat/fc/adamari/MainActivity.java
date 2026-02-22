27f7c84ab2a6ae6d4ef2e2a9a8dc377d6c01d41d
adamariglc <adamariglc@ciencias.unam.mx>
2026-02-21 20:02:39 -0600

    quite algunas cosas que me faltaban

--- app/src/main/java/mx/unam/icat/fc/adamari/MainActivity.java
+++ app/src/main/java/mx/unam/icat/fc/adamari/MainActivity.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-/**git
+/**
  * @author <a href=adamariglc@ciecnias.unam.mx> Adamari Gianina Lopez Cortes - @adamariglc </a>
  */
 
